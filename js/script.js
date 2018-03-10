692d7e750f9c48652c3b9c00db4617362a58b48d
Hauke Grothues <hauke.grothues@gmail.com>
2018-03-10 14:37:14 +0100

    Starting the hustle!
    Added first functions to code to make field clickable.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
   // declare variables
-
+  var playersTurn = "Player1";
+  var fieldsArr = [];
+  ($(".field").click(function() {
+    var pickedField = $(this);
+    if (fieldsArr.indexOf(pickedField.attr('class').split(' ')[1]) == -1) {
+      fieldsArr.push(pickedField.attr('class').split(' ')[1]);
+      switch(playersTurn) {
+        case "Player1":
+          $(pickedField).css("background-color", "orange");
+          playersTurn = "Player2";
+          break;
+        case "Player2":
+          $(pickedField).css("background-color", "green");
+          playersTurn = "Player1";
+          break;
+      }
+    }
+  }));
 });
