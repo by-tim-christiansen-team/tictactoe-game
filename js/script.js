f6e63a24ee172977c2be7baff1be4ff573d0e08c
Hauke Grothues <hauke.grothues@gmail.com>
2018-04-11 09:34:08 +0200

    add more theory stuff

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
 $(document).ready(function(){
   // declare variables
   var gameData = {
-    "playerCharacter": "",
-    "unavailableFields": [],
-    "score": {"wins": 0,
-              "ties": 0,
-              "losses": 0
-            }
+    "playerChar": "X",
+    "score": {
+      "wins": 0,
+      "ties": 0,
+      "losses": 0
+    },
+    "winArr": [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ],
+    "fieldsIndex": {
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+      6: "",
+      7: "",
+      8: "",
+    }
   }
 
   $(".field").click(function() {
     var pickedField = $(this).attr("class").split(" ")[1];
-    if (gameData.unavailableFields.indexOf(pickedField) == -1) {
-      gameData.unavailableFields.push(pickedField);
+    if (gameData.fieldsIndex[pickedField] == "") {
+      gameData.fieldsIndex[pickedField] = "player";
       fillField(pickedField);
+      checkWin();
       console.log("Field available!");
-      console.log(gameData.unavailableFields);
+      console.log(gameData.fieldsIndex);
     }
     else {
       console.log("Field unavailable!");
@@ -23,5 +44,11 @@ $(document).ready(function(){
   });
 
   function fillField(field) {
+    $("." + field).text(gameData.playerChar);
   }
-});
+
+  function checkWin() {
+    for (var h = 0; h < gameData.winArr[h].length)
+  }
+
+ });
