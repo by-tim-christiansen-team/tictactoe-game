bf096208ccace80893ab55b46d969dd87630ab87
Hauke Grothues <hauke.grothues@gmail.com>
2018-04-13 16:25:32 +0200

    Delete useless lines

--- js/script.js
+++ js/script.js
@@ -128,12 +128,9 @@ $(document).ready(function(){
   }
 
   function minimax(newBoard, player){
-  //add one to function calls
 
-  //available spots
   var availSpots = emptyIndexies(newBoard);
 
-  // checks for the terminal states such as win, lose, and tie and returning a value accordingly
   if (checkWin(newBoard, "player")){
      return {score:-10};
   }
@@ -144,19 +141,15 @@ $(document).ready(function(){
   	return {score:0};
   }
 
-// an array to collect all the objects
   var moves = [];
 
-  // loop through available spots
   for (var i = 0; i < availSpots.length; i++){
-    //create an object for each and store the index of that spot that was stored as a number in the object's index key
+
     var move = {};
   	move.index = newBoard[availSpots[i]];
 
-    // set the empty spot to the current player
     newBoard[availSpots[i]] = player;
 
-    //if collect the score resulted from calling minimax on the opponent of the current player
     if (player == "comp"){
       var result = minimax(newBoard, "player");
       move.score = result.score;
@@ -166,14 +159,11 @@ $(document).ready(function(){
       move.score = result.score;
     }
 
-    //reset the spot to empty
     newBoard[availSpots[i]] = move.index;
 
-    // push the object to the array
     moves.push(move);
   }
 
-// if it is the computer's turn loop over the moves and choose the move with the highest score
   var bestMove;
   if(player === "comp"){
     var bestScore = -10000;
@@ -183,9 +173,8 @@ $(document).ready(function(){
         bestMove = i;
       }
     }
-  }else{
+  } else{
 
-// else loop over the moves and choose the move with the lowest score
     var bestScore = 10000;
     for(var i = 0; i < moves.length; i++){
       if(moves[i].score < bestScore){
@@ -195,7 +184,6 @@ $(document).ready(function(){
     }
   }
 
-// return the chosen move (object) from the array to the higher depth
 return moves[bestMove];
 }
 });
